c764ea442dc75b1047997f0a38fd3312ad9c5206
Adam Pearce <adampearce@adampearce-macbookpro2.roam.corp.google.com>
2018-12-28 00:47:57 -0500

    saving works

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ var querystring = require("querystring")
 
 var PORT = require('minimist')(process.argv.slice(2)).port || 3989
 
-
-
 var injectHTML = fs.readFileSync(__dirname + '/inject.html', 'utf8')
 
 // TODO limit to localhost
@@ -18,39 +16,63 @@ var injectHTML = fs.readFileSync(__dirname + '/inject.html', 'utf8')
 
 // set up express static server with a websocket
 var server = express()
-  // TODO server editor.html with node paths allowed
-  // TODO save files that are changed
+  // .get('hot-editor.html', (req, res, next) => {
+    
+  // })
+  // save files that are changed
+  .post('/save', (req, res, next) => {
+    var bodyStr = ''
+    req
+      .on('data', d => bodyStr += d )
+      .on('end', () => {
+        var {path, str} = JSON.parse(bodyStr)
+
+        console.log('saving', path)
+        fs.writeFileSync(path, str)
+        res.end()
+      })
+  })
   .get('*', (req, res, next) => {
+    var path = req.params[0].slice(1)
+    
+    // server editor.html with node paths
+    // if (path.includes('hot-editor.html')){
+    //   return res.send(fs.readFileSync(__dirname + '/hot-editor.html', 'utf8'))
+    // }
+    // if (path.includes('node_modules/codemirror/')){
+    //   res.contentType(path)
+    //   return res.send(fs.readFileSync(__dirname + '/' + path))
+    // }
+
     // append injecter script to all html pages served
     try{
-      var path = req.params[0].slice(1)
       if (path.slice(-1) == '/') path = path + '/index.html'
       if (path == '') path = 'index.html'
       if (path.slice(-5) != '.html') return next()
 
-      res.send(fs.readFileSync(path, 'utf-8') + injectHTML)
+      return res.send(fs.readFileSync(path, 'utf-8') + injectHTML)
     } catch(e){ next() }
   })
   .use(serveStatic('./'))
   .use('/', serveIndex('./'))
   .listen(PORT)
-  .on('listening', () => child.exec('open http://localhost:' + PORT))
+  .on('listening', () => {
+    var url = 'http://localhost:' + PORT
+    child.exec('open ' + url)
+    console.log('hot-server: ' + url)
+    console.log(url + '/hot-editor?hotEditorPath=_script.js')
+  })
   
 process.on('uncaughtException', (err => 
   err.errno == 'EADDRINUSE' ? server.listen(++PORT) : 0)) //inc PORT if in use
 
 
 
-console.log({PORT})
+// 
 
+// var result = querystring.stringify({hotEditorPath: "_script.js"})
+// console.log(result)
 
-
-var result = querystring.stringify({hotEditorPath: "_script.js"})
-console.log(result)
-
-
-
-console.log({PORT})
 
 
 
